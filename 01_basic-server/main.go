07d935b5f4e941916d9230df60d42baffb49bc28
andy <andyalam@hotmail.com>
2017-02-02 11:12:47 -0800

    iterating towards basic CRUD file server

--- 01_basic-server/main.go
+++ 01_basic-server/main.go
@@ -2,16 +2,65 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"io/ioutil"
 	"net/http"
 )
 
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
 func main() {
-	http.HandleFunc("/", index)
+	/*const pageTitle string = "TestPage"
+	p1 := &Page{
+		Title: pageTitle,
+		Body:  []byte("This is a sample Page."),
+	}
+	p1.save()
+
+	p2, _ := loadPage(pageTitle)
+	fmt.Println(string(p2.Body)) */
+
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-	title := "hello"
-	body := "asdsadasdsa"
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", title, body)
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+
+	// use template
+	t, _ := template.ParseFiles("edit.html")
+	t.Execute(w, p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+
 }
